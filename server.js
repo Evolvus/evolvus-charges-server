c7bd6b9f5691fa6c7e59396b02422a085356ca39
Kavya K M <EVOLVUS\kavyak@elpblr-083.evolvus.com>
2018-10-31 11:22:10 +0530

    updated scheduler job

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ const debug = require("debug")("evolvus-charges-server:server");
 const express = require("express");
 const bodyParser = require("body-parser");
 const connection = require("@evolvus/evolvus-mongo-dao").connection;
-const axios=require("axios");
+const axios = require("axios");
 const app = express();
 const router = express.Router();
+const moment = require("moment");
 
 var dbConnection = connection.connect("CHARGES");
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   // res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Request-Headers", "*");
   res.header('Access-Control-Allow-Methods', 'GET, POST,PUT, DELETE, OPTIONS');
@@ -51,15 +52,29 @@ app.use("/api", router);
 */
 var schedule = require('node-schedule');
 
-var j = schedule.scheduleJob('30 * * * * * ', function(){
-  axios.post("http://192.168.1.100:9292/api/generateBill",{},{headers:{
-    "X-USER":"SYSTEM",
-    "X-IP-HEADER":"192.168.1.100"
-  }}).then((res)=> {
-    console.log(res);
-  }).catch(e=> {
-    console.log(e)
-  })
+var date = new Date();
+var d = moment(date).subtract(1, 'month');
+d.month();
+var billPeriod = d.format('MMMYYYY');
+var toDate = moment(date).format("DD-MM-YYYY");
+date.setMonth(date.getMonth() - 1);
+var fromDate = moment(date).format("DD-MM-YYYY");
+
+var j = schedule.scheduleJob('30 * * * * * ', function () {
+  axios.post("http://192.168.1.100:9292/api/generateBill", {
+    billPeriod: billPeriod,
+    fromDate: fromDate,
+    toDate: toDate
+  }, {
+      headers: {
+        "X-USER": "SYSTEM",
+        "X-IP-HEADER": "192.168.1.100"
+      }
+    }).then((res) => {
+      debug(res.data);
+    }).catch(e => {
+      debug(e);
+    })
 });
 
 const server = http.createServer(app);
